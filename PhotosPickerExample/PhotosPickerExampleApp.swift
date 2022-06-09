480acc18c1688f23d19bed7917d680a8076b9fd7
Russell Gordon <github@russellgordon.ca>
2022-06-09 15:20:18 -0400

    Initial Commit

---
+++ PhotosPickerExample/PhotosPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerExampleApp.swift
+//  PhotosPickerExample
+//
+//  Created by Russell Gordon on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
